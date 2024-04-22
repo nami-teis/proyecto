ef7ed7d4b4dc22ef83d7667e0a5bcf39298bef0a
Nadia Marquez <nadiamarquez27112005@gmail.com>
2024-04-22 19:13:27 +0200

    Creación del proyecto

---
+++ src/proyecto/Sumador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author nadia
+ */
+public class Sumador {
+
+    public static float sumar(float a, float b) {
+        return a + b;
+    }
+}
